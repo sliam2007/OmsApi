628de10cb92f5cf8041d4215bcae692e93260a5d
Evgeny Slemzin <sliam2007@gmail.com>
2021-12-12 16:32:00 +0100

    DEL - remove comments

--- Data/CreateOrder.cs
+++ Data/CreateOrder.cs
@@ -6,11 +6,11 @@ namespace OmsApi.Data
     public class CreateOrder
     {
 
-        public string omsId { get; set; }                   // Unique OMS identifier.
+        public string omsId { get; set; }                   
 
-        public IList<OrderProduct> products { get; set; }   // List of products.
+        public IList<OrderProduct> products { get; set; }   
 
-        public OrderDetails orderDetails { get; set; }      // Additional order information. 
+        public OrderDetails orderDetails { get; set; }       
 
     }
 }
